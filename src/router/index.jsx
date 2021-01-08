01817924057dcc77b8ff2db93d229e4546128f8f
Your Name <mrborbaivan@gmail.com>
2021-01-08 12:47:58 -0300

    Initial commit

---
+++ src/router/index.jsx
@@ -0,0 +1,11 @@
+import { Route, Switch } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/"></Route>
+    </Switch>
+  );
+};
+
+export default Router;
