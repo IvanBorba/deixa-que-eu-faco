1b03c6a5f1d01cf36731553672ce221f96f5e5a6
Alexandre Alves <alexandrealvescs@gmail.com>
2021-01-12 15:04:20 -0300

    estrutura básica do login + url da img

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Background } from "./styled";
+
+const Login = () => {
+  return (
+    <>
+      <Background></Background>
+    </>
+  );
+};
+
+export default Login;
