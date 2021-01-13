a9c423eee5806781f46fff5d3a824b0d129e2074
Mariana Antunes <marimsantunes@gmail.com>
2021-01-13 11:21:14 -0300

    add 2 footer sections and logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Router from "./router";
+import Footer from "./components/footer";
 
 const App = () => {
   return (
     <div>
       <h1>Deixa que eu faço</h1>
       <Router />
+      <Footer />
     </div>
   );
 };
