451f4f32600ff86fa57595d405b55d6004a56a2e
HowardRicardo <howard.medeiros@gmail.com>
2021-01-16 19:21:29 -0300

    responsive/desktop registerPage

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -4,27 +4,32 @@ import "./index.css";
 import logo from "../../images/logo/logo.png";
 import RegisterForm from "../../components/forms/register-form";
 import { useHistory } from "react-router-dom";
+import customerImage from "../../images/register/registerImage.jpg";
+import cheffImage from "../../images/register/cheff.jpeg";
 
 const RegisterPage = () => {
     const [chefRegister, setChefRegister] = useState(false);
 
     const history = useHistory();
     return (
-        <div className={!chefRegister ? "registerBox" : "registerBoxCheff"}>
-            <div className="Film">
-                <div className="topButtons" >
-                    <Button style={{color: "#9E5642"}} onClick={() => history.push("/")}>VOLTAR</Button>
-                    <Button style={{color: "#9E5642"}} onClick={() => history.push("/login")}>LOGIN</Button>
-                </div>
-                <div className={!chefRegister ? "theForm" : "theFormChef"}>
-                    <img alt="carregando" src={logo} className={!chefRegister ? "registerLogo" : "registerLogoChef"}/>
-                    <span style={!chefRegister ? {position: "relative", top: 25, color: "#9E5642", transition: "1s"} : {position: "relative", top: -30, color: "#9E5642", transition: "1s"}}>CADASTRE-SE COMO {!chefRegister ? "CLIENTE" : "CHEFE"}</span>
-                    <RegisterForm chefRegister={chefRegister} setChefRegister={setChefRegister}/>
-                </div>
+        <div className="pageBox">
+            <img src={!chefRegister ? customerImage : cheffImage} alt="" className="desktopImage"/>
+            <div className={!chefRegister ? "registerBox" : "registerBoxCheff"}>
+                <div className="Film">
+                    <div className="topButtons" >
+                        <Button style={{color: "#9E5642"}} onClick={() => history.push("/")}>VOLTAR</Button>
+                        <Button style={{color: "#9E5642"}} onClick={() => history.push("/login")}>LOGIN</Button>
+                    </div>
+                    <div className={!chefRegister ? "theForm" : "theFormChef"}>
+                        <img alt="carregando" src={logo} className={!chefRegister ? "registerLogo" : "registerLogoChef"}/>
+                        <span style={!chefRegister ? {position: "relative", top: 25, color: "#9E5642", transition: "1s"} : {position: "relative", top: -30, color: "#9E5642", transition: "1s"}}>CADASTRE-SE COMO {!chefRegister ? "CLIENTE" : "CHEFE"}</span>
+                        <RegisterForm chefRegister={chefRegister} setChefRegister={setChefRegister}/>
+                    </div>
 
-                <div className="registerFooter">
-                    <span>DEIXA QUE EU FAÇO</span>
-                    <span>© Todos os direitos reservados.</span>
+                    <div className="registerFooter">
+                        <span>DEIXA QUE EU FAÇO</span>
+                        <span>© Todos os direitos reservados.</span>
+                    </div>
                 </div>
             </div>
         </div>
