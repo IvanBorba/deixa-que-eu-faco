2875c404bf519394d1d57f2695c45a7a5eac33b7
Alexandre Alves <alexandrealvescs@gmail.com>
2021-01-19 18:53:41 -0300

    ADD funcionalidade de quando o serviço for 0 renderizar na tela mensagem dizendo que não possui serviço

--- src/components/card-services-chef/services-status/index.jsx
+++ src/components/card-services-chef/services-status/index.jsx
@@ -1,11 +1,35 @@
 import React from "react";
-import { Box } from "./styled";
+import { Box, BoxAlert } from "./styled";
 import CardServicesChef from "../card";
 
 const Services = ({ list, option }) => {
   const servicesFinished = list.filter((item) => item.status === "finished");
   const servicesWaiting = list.filter((item) => item.status === "waiting");
   const servicesAccepted = list.filter((item) => item.status === "accepted");
+
+  const nothingInfoFinished = (service) => {
+    if (option === "finished" && service.length === 0) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const nothingInfoAccepted = (service) => {
+    if (option === "accepted" && service.length === 0) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const nothingInfoWaiting = (service) => {
+    if (option === "waiting" && service.length === 0) {
+      return true;
+    }
+
+    return false;
+  };
 
   return (
     <Box>
@@ -21,6 +45,16 @@ const Services = ({ list, option }) => {
           servicesWaiting.map((item, index) => {
             return <CardServicesChef key={index} card={item} />;
           })}
+
+      {nothingInfoWaiting(servicesWaiting) ? (
+        <BoxAlert>Nenhum serviço pendente</BoxAlert>
+      ) : nothingInfoAccepted(servicesAccepted) ? (
+        <BoxAlert>Nenhum serviço agendado</BoxAlert>
+      ) : (
+        nothingInfoFinished(servicesFinished) && (
+          <BoxAlert>Nenhum serviço finalizado</BoxAlert>
+        )
+      )}
     </Box>
   );
 };
