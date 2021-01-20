9e0f33835a7d8e5b92684a78f8b5a8e6519c0f34
Your Name <mrborbaivan@gmail.com>
2021-01-20 00:37:57 -0300

    last fix

--- src/pages/user-settings/index.jsx
+++ src/pages/user-settings/index.jsx
@@ -1,27 +1,34 @@
-import { SettingsAvatar, SettingsDatas, LoginSettings, SaveConfigs } from "../../components/forms/settings-form";
+import {
+  SettingsAvatar,
+  SettingsDatas,
+  LoginSettings,
+  SaveConfigs,
+} from "../../components/forms/settings-form";
 import "./index.css";
-const isChef = JSON.parse(localStorage.getItem("userData")).isChef
 
 const SettingsPage = () => {
-    return (
-        <div className="settingsPage">
-            <div className="subHeader">
-                <h4>EDITAR O PERFIL > {isChef ? "COZINHEIRO" : "CLIENTE"}</h4>
-            </div>
-            <div>
-                <SettingsAvatar/>
-            </div>
-            <div>
-                <SettingsDatas />
-            </div>
-            <div>
-                <LoginSettings />
-            </div>
-            <div>
-                <SaveConfigs />
-            </div>
-        </div>
-    )
-}
+  const isChef = JSON.parse(localStorage.getItem("userData")).isChef;
+  return (
+    <div className="settingsPage">
+      <div className="subHeader">
+        <h4>
+          EDITAR O PERFIL {">"} {isChef ? "COZINHEIRO" : "CLIENTE"}
+        </h4>
+      </div>
+      <div>
+        <SettingsAvatar />
+      </div>
+      <div>
+        <SettingsDatas />
+      </div>
+      <div>
+        <LoginSettings />
+      </div>
+      <div>
+        <SaveConfigs />
+      </div>
+    </div>
+  );
+};
 
-export default SettingsPage
+export default SettingsPage;
