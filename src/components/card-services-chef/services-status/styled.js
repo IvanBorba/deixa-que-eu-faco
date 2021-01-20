7944f4989593d2490cb72ed94ea7b39ed7695ee1
Alexandre Alves <alexandrealvescs@gmail.com>
2021-01-20 17:36:25 -0300

    Ajustes de espaçamento, ADD infos de preço e número de pessoas, ADD cursor pointer e um título para cada serviço

--- src/components/card-services-chef/services-status/styled.js
+++ src/components/card-services-chef/services-status/styled.js
@@ -4,13 +4,24 @@ export const Box = styled.div`
   display: flex;
   flex-flow: column;
   align-items: center;
+
+  h1 {
+    margin: 15px;
+  }
+`;
+
+export const BoxContent = styled.div`
+  margin: 25px;
+
+  @media (min-width: 1024px) {
+    min-height: 500px;
+  }
 `;
 
 export const BoxAlert = styled.div`
-  padding: 200px 0;
-  margin: 10px;
   color: #aa4658;
   text-align: center;
+  margin: 50px;
 
   @media (min-width: 768px) {
     font-size: 25px;
@@ -19,5 +30,6 @@ export const BoxAlert = styled.div`
   @media (min-width: 1440px) {
     padding: 300px;
     font-size: 30px;
+    margin: auto;
   }
 `;
